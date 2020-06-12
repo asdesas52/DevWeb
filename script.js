e14fb383afcac87eff383f7f9e8c5d44ff57e226
asdesas52 <57477852+asdesas52@users.noreply.github.com>
2020-06-12 11:16:11 +0200

    Ex_4

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function testFonctionnalites() {
-    document.querySelector("#geoloc").innerHTML = Modernizr.geolocation ? "pris en charge" : "non pris en charge";
-    document.querySelector("#touch").innerHTML = Modernizr.touch ? "pris en charge" : "non pris en charge";
-    document.querySelector("#svg").innerHTML = Modernizr.svg ? "pris en charge" : "non pris en charge";
-    document.querySelector("#canvas").innerHTML = Modernizr.canvas ? "pris en charge" : "non pris en charge";
+function initialize() { 
+    var bSupportsLocal = (('localStorage' in window) && window['localStorage'] !== null);
+    if (!bSupportsLocal) {
+        document.getElementById('infoform').innerHTML = "<p>Désolé, ce navigateur ne supporte pas l’API Web Storage du W3C.</p>";
+        return;
+    }
+    if (window.localStorage.length != 0) {
+        document.getElementById('firstName').value = window.localStorage.getItem('firstName');
+        document.getElementById('lastName').value = window.localStorage.getItem('lastName');
+        document.getElementById('postCode').value = window.localStorage.getItem('postCode');
+    }
 }
-window.onload = testFonctionnalites;
+
+function storeLocalContent(fName, lName, pCode) {
+    window.localStorage.setItem('firstName', fName);
+    window.localStorage.setItem('lastName', lName);
+    window.localStorage.setItem('postCode', pCode);
+}
+
+function clearLocalContent(){
+    localStorage.clear();
+}
+
+window.onload = initialize;
