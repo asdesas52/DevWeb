1eb4c93ff0e035d6a8060b6369938511ef801543
asdesas52 <57477852+asdesas52@users.noreply.github.com>
2020-06-12 11:16:43 +0200

    Ex_5

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-function initialize() { 
-    var bSupportsLocal = (('localStorage' in window) && window['localStorage'] !== null);
-    if (!bSupportsLocal) {
-        document.getElementById('infoform').innerHTML = "<p>Désolé, ce navigateur ne supporte pas l’API Web Storage du W3C.</p>";
-        return;
-    }
-    if (window.localStorage.length != 0) {
-        document.getElementById('firstName').value = window.localStorage.getItem('firstName');
-        document.getElementById('lastName').value = window.localStorage.getItem('lastName');
-        document.getElementById('postCode').value = window.localStorage.getItem('postCode');
+function getLocation() { 
+    if (Modernizr.geolocation) {
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+        
     }
 }
 
-function storeLocalContent(fName, lName, pCode) {
-    window.localStorage.setItem('firstName', fName);
-    window.localStorage.setItem('lastName', lName);
-    window.localStorage.setItem('postCode', pCode);
+function geoSuccess (positionInfo){
+    document.getElementById("longitude").innerHTML = positionInfo.coords.longitude;
+    document.getElementById("latitude").innerHTML = positionInfo.coords.latitude;
+    document.getElementById("precision").innerHTML = positionInfo.coords.accuracy;
+    document.getElementById("altitude").innerHTML = positionInfo.coords.altitude;
+    document.getElementById("precisionAltitude").innerHTML = positionInfo.coords.altitudeAccuracy;
+    document.getElementById("cap").innerHTML = positionInfo.coords.heading;
+    document.getElementById("vitesse").innerHTML = positionInfo.coords.speed;
+    var Esirem={"longitude":5.0039326,"latitude":47.3121519};
+    document.getElementById("distance").innerHTML = calculDistance(positionInfo.coords, Esirem);
 }
 
-function clearLocalContent(){
-    localStorage.clear();
+
+function geoError (positionError){
+if (errorInfo.code == 1)
+    alert("L’utilisateur ne souhaite pas partager sa position");
+else if (errorInfo.code == 2)
+         alert("Impossible de déterminer une position")
+else if ( errorInfo.code == 3)
+    alert("Délai de recherche de position trop long");
 }
 
-window.onload = initialize;
+function calculDistance (startCoords, destCoords){
+    var startLatRads = degreesEnRadians(startCoords.latitude);
+    var startLongRads = degreesEnRadians(startCoords.longitude);
+    var destLatRads = degreesEnRadians(destCoords.latitude);
+    var destLongRads = degreesEnRadians(destCoords.longitude);
+    var Radius = 6371;// rayon de la Terre en km
+    var distance = Math.acos(Math.sin(startLatRads)*Math.sin(destLatRads)+Math.cos(startLatRads)*Math.cos(destLatRads)+Math.cos(startLongRads-destLongRads))*Radius;
+    return distance;
+}
+
+function degreesEnRadians(degrees){
+    radians =(degrees * Math.PI)/180;
+    return radians;
+}
+ 
+window.onload = getLocation;
